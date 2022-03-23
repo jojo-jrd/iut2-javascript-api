acb4299a57c18551a5855fce9146f532ea254e16
Jordan Josserand <jordan.josserand@etu.univ-grenoble-alpes.fr>
2022-03-23 12:44:32 +0100

    SupprimerFavoris fonctionel

--- scripts/script_principal.js
+++ scripts/script_principal.js
@@ -5,7 +5,7 @@ function init(){
     localStorage.setItem("Test",JSON.stringify(testTableau));
     console.log(JSON.parse(localStorage.getItem("Test")));
     */
-    var testTableau = {"a":"a","b":"b"};
+    var testTableau = {"1":"a","2":"b"};
     localStorage.setItem("favoris",JSON.stringify(testTableau));
     var favoris = localStorage.getItem("favoris");
 
@@ -17,20 +17,34 @@ function init(){
     */
     if(favoris!=null){
         var fav = JSON.parse(favoris);
-        $("#liste-favoris").empty();
+        //$("#liste-favoris").empty();
         for(var f in fav){ // Parcourt des clés des favoris
+            $("#liste-favoris").append("<li id="+f+"></li>");
+            $("#"+f).append("<span title=\"Cliquer pour relancer la recherche\">"+fav[f]+"</span>");
+            $("#"+f).append("<img src=\"images/croix.svg\" alt=\"Icone pour supprimer le favori\" width=15 title=\"Cliquer pour supprimer le favori\" onClick=\"supprimerFavoris("+f+")\">");
             console.log(f)
         }
     }
 }
 
 
-function ajouterFavoris(){
-
-    localStorage.setItem("f")
+function ajouterFavoris(id){
+    
 
 }
 
-function supprimerFavoris(){
-    
+function supprimerFavoris(id){
+    // Voir les indices
+    $("#"+id).remove();
+    var favoris = localStorage.getItem("favoris");
+    if(favoris!=null){
+        var fav = JSON.parse(favoris);
+        for(var f in fav){
+            if(f==id){
+                delete fav[f];
+            }
+        }
+        localStorage.setItem("favoris",JSON.stringify(fav))
+        console.log("Supprimer "+id);
+    }
 }
