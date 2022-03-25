700d03029ef6115333d6e6a94086eacdb405cc97
guicluca <lucas.guichard@etu.univ-grenoble-alpes.fr>
2022-03-25 23:49:17 +0100

    traitement image api

--- scripts/api_interaction.js
+++ scripts/api_interaction.js
@@ -27,7 +27,11 @@ function debug(msg, param){
 
 // API ENDPOINT :
 var API_ALL_CHAMPIONS = "https://ddragon.leagueoflegends.com/cdn/12.5.1/data/fr_FR/champion.json";
-var API_NAME_CHAMPION = "https://ddragon.leagueoflegends.com/cdn/12.5.1/data/fr_FR/champion/";
+var API_NAME_CHAMPION = "https://ddragon.leagueoflegends.com/cdn/12.5.1/data/fr_FR/champion/";      // + Nomchamp.json
+var API_IMAGE_SPLASH = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/";               // + Nomchamp_X.jpg
+var API_IMAGE_LOADING = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/";             // + Nomchamp_X.jpg
+var API_IMAGE_PASSIV = "http://ddragon.leagueoflegends.com/cdn/12.5.1/img/passive/"                 // + Nomchamp_P.png
+var API_IMAGE_SPELL = "http://ddragon.leagueoflegends.com/cdn/12.5.1/img/spell/";                   // + SpellName.png
 
 // SPECIAL REQUEST PARAM :
 var NAME_ONLY = 0           // => récupère seuleument le/les noms
@@ -60,11 +64,10 @@ function requestChampionsList(param){
         "json");
 }
 
-
 /**
  * Traite et retourne la liste de tous les objet JSON des champions
  * dans un array Javascript
- * @returns array<JSON_OBJ>
+ * @returns {array<JSON_OBJ>}
  */
 function getChampionsList(data){
     debug("Entré dans la fonciton getChampionsList. data = ",[data]);
@@ -76,7 +79,7 @@ function getChampionsList(data){
 /**
  * Traite et retourne la liste de tous les noms des champions
  * dans un array Javascript
- * @returns array<STRING>
+ * @returns {array<STRING>}
  */
  function getNameList(data){
     debug("Entré dans la fonciton getNameList. data = ",[data]);
@@ -119,7 +122,7 @@ function getChampionsList(data){
  * Retourne un objet json contenant toutes les informations sur
  * le champion.
  * 
- * @returns Array<JSON_OBJ>
+ * @returns {Array<JSON_OBJ>}
  */
  function getChampionInfoByName(data){
     debug("Entré dans la fonction getChampionInfoByName. data =", [data]);
@@ -127,3 +130,88 @@ function getChampionsList(data){
     debug("Retourne : ", [ChampionInfo]);
     return ChampionInfo;
 }
+
+
+// ================ IMAGE FROM CHAMP INFO ===============
+
+function loadOBJTMP() {
+    var address = API_NAME_CHAMPION + "Aatrox.json";
+    $.get(address,
+        (data) => {
+            getLoadingFromObj(data);
+            getSplashFromObj(data);
+            
+        },
+        "json");
+  }
+
+/**
+ * Retourne les liens des images splash à partir 
+ * d'un obj json de champion
+ * 
+ * @param {JSON_OBJ} obj 
+ * @returns {Array<{'name':String, 'link':String}>}
+ */
+function getSplashFromObj(data){
+    var obj = Object.values(data.data)[0];
+    debug("Entré dans la fonction getSplashFromObj. obj =", [obj]);
+    var NameAndSplash = [];
+    var skinList = obj.skins;
+    Object.keys(skinList).forEach((key)=>{
+        let skin = skinList[key];
+        let name = skin.name;
+        let link = API_IMAGE_SPLASH + obj.id + "_" + key +".jpg"; // api/NomChamp_X.jpg
+        NameAndSplash.push({'name': name, 'link': link});
+    });
+    debug("Retourne le tableau de splash :", [NameAndSplash]);
+
+    return NameAndSplash;
+}
+
+/**
+ * Retourne les liens des images loadings à partir 
+ * d'un obj json de champion
+ * 
+ * @param {JSON_OBJ} obj 
+ * @returns {Array<{'name':String, 'link':String}>}
+ */
+function getLoadingFromObj(data){
+    var obj = Object.values(data.data)[0];
+    debug("Entré dans la fonction getLoadingFromObj. obj =", [obj]);
+    var NameAndLoading = [];
+    var skinList = obj.skins;
+    Object.keys(skinList).forEach((key)=>{
+        let skin = skinList[key];
+        let name = skin.name;
+        let link = API_IMAGE_LOADING + obj.id + "_" + key +".jpg"; // api/NomChamp_X.jpg
+        NameAndLoading.push({'name': name, 'link': link});
+    });
+    debug("Retourne le tableau de Loading :", [NameAndLoading]);
+
+    return NameAndLoading;
+}
+
+/**
+ * Retourne un tableau d'objet json contenant le nom de chaque spell
+ * et le lien vers son image assoscié.
+ * @param {JSON_OBJ} obj  
+ * @returns {Array<{name : String, link : String}>} 
+ */
+function getSpellAndImageFromObj(data){
+    var obj = Object.values(data.data)[0];
+    debug("Entré dans la fonction getSpellAndImageFromObj. obj=", [obj]);
+    var NameAndImageList = [];
+    var spellList = obj.spells;
+    Object.keys(spellList).forEach(function(key){
+        let spell = spellList[key];
+        let name = spel.name;
+        let link = API_IMAGE_SPELL + spell.image.full;
+        NameAndImageList.push({"name": name, "link": link});
+      }); 
+      
+    debug("Retourne le tableau :", NameAndImageList);
+    return NameAndImageList;
+}
+
+ 
+// 4 - passiv
