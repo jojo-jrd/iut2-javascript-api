19b524fdd40ae7b5f02253308e3aaa208d5abec7
PC 30 Marco Grenoble <marco.vassal@etu.univ-grenoble-alpes.fr>
2022-03-29 17:17:34 +0200

    script carte + resultat done

--- scripts/retour_recherche.js
+++ scripts/retour_recherche.js
@@ -1,97 +1,96 @@
 function afficheResultat(nameChamp){
-
-    var champion = getTemplateResultatInfo(nameChamp);
+    getTemplateResultatInfo(nameChamp).then((champion) => {
+        
+        console.log(champion.splashs);
+        var divRoleResultat = $(".role_resultat");
+        var divNameResultat = $(".name_resultat");
+        var divStatResultat = $(".stat_resultat");
+        var divLoreResultat = $(".lore_resultat");
     
-    var divRoleResultat = $(".role_resultat");
-    var divNameResultat = $(".name_resultat");
-    var divStatResultat = $(".stat_resultat");
-    var divLoreResultat = $(".lore_resultat");
-
-    $(".main_sec").css("background-image", "url(" + (champion.splashs[0]).link + ")");
-
-    champion.roles.forEach((tag) =>  {
-        divRoleResultat.append($("<p>" + tag + "</p>"));
+        $(".main_sec").css("background-image", "url(" + (champion.splashs[0]).link + ")");
+    
+        champion.roles.forEach((tag) =>  {
+            divRoleResultat.append($("<p>" + tag + "</p>"));
+        });
+        divNameResultat.prepend($("<h1>" + champion.name + "</h1>"));
+    
+        divNameResultat.append($("<p>" + champion.difficulty + "</p>"));
+    
+        divStatResultat.append($("<p>" + champion.stats.HP + " HP</p>"));
+        divStatResultat.append($("<p>" + champion.stats.DMG + " Dmg</p>"));
+    
+        divLoreResultat.append($("<p>" + champion.lore + "</p>"));
     });
-    divNameResultat.prepend($("<h1>" + champion.name + "</h1>"));
-
-    divNameResultat.append($("<p>" + champion.difficulty + "</p>"));
-
-    divStatResultat.append($("<p>" + champion.stats.HP + " HP</p>"));
-    divStatResultat.append($("<p>" + champion.stats.DMG + " Dmg</p>"));
-
-    divLoreResultat.append($("<p>" + champion.lore + "</p>"));
 }
 
 
 function afficheCarte(nameChamp){
 
-    var champion = getTemplateCarteInfo(nameChamp);
+    getTemplateCarteInfo(nameChamp).then((champion) => {
+        var divTitreCarte      = $(".titre"           );
+        var divRoleCarte       = $(".roles"           );
+        var divStatCarte       = $(".stats"           );
+        var divDiffCarte       = $(".difficult"       );
+        var divSkinsCarte      = $(".skins"           );
+        var divSpellsCarte     = $(".spells"          );
+        var secPassifCarte     = $(".passif"          );
+        var divLoreCarte       = $(".lore"            );
 
-    var divTitreCarte      = $(".titre"           );
-    var divRoleCarte       = $(".roles"           );
-    var divStatCarte       = $(".stats"           );
-    var divDiffCarte       = $(".difficult"       );
-    var divSkinsCarte      = $(".skins"           );
-    var divSpellsCarte     = $(".spells"          );
-    var secPassifCarte     = $(".passif"          );
-    var divLoreCarte       = $(".lore"            );
-    
-    //SECTION carte_principale
-    $(".main_sec").css("background-image", "url(" + (champion.loadings[0]).link + ")");
+        //SECTION carte_principale
+        $(".main_sec").css("background-image", "url(" + (champion.loadings[0]).link + ")");
 
-    //DIV titre
-    divTitreCarte.append($("<h1>" + champion.name + "</h1>"))
-    divTitreCarte.append($("<h4>" + champion.title + "</h4>"))
-    
-    //DIV roles
-    champion.roles.forEach((tag) =>  {
-        divRoleCarte.append($("<p>" + tag + "</p>"));
-    });
+        //DIV titre
+        divTitreCarte.append($("<h1>" + champion.name + "</h1>"));
+        divTitreCarte.append($("<h4>" + champion.title + "</h4>"));
 
-    //DIV stats
-    divStatCarte.append($("<p>HP : " + champion.HP + "</p>"));
-    divStatCarte.append($("<p>Vitesse de déplacement : " + champion.MOVESPEED + "</p>"));
-    divStatCarte.append($("<p>Armure : " + champion.ARMOR + "</p>"));
-    divStatCarte.append($("<p>Rayon d'attaque : " + champion.RANGE + "</p>"));
-    divStatCarte.append($("<p>Dégats d'attaque : " + champion.DMG + "</p>"));
+        //DIV roles
+        champion.roles.forEach((tag) =>  {
+            divRoleCarte.append($("<p>" + tag + "</p>"));
+        });
 
-    //DIV difficult
-    divDiffCarte.append($("<p>Difficulté : " + champion.DIFFICULTY + "</p>"));
-    divDiffCarte.append($("<p>Attaque : " + champion.ATK + "</p>"));
-    divDiffCarte.append($("<p>Défense : " + champion.DEF + "</p>"));
-    divDiffCarte.append($("<p>Magie : " + champion.MAG + "</p>"));
+        //DIV stats
+        divStatCarte.append($("<p>HP : " + champion.HP + "</p>"));
+        divStatCarte.append($("<p>Vitesse de déplacement : " + champion.MOVESPEED + "</p>"));
+        divStatCarte.append($("<p>Armure : " + champion.ARMOR + "</p>"));
+        divStatCarte.append($("<p>Rayon d'attaque : " + champion.RANGE + "</p>"));
+        divStatCarte.append($("<p>Dégats d'attaque : " + champion.DMG + "</p>"));
 
-    //DIV skins
-    champion.skins.forEach((skin) =>  {
-        divSkinsCarte.append($("<div><img src=\"" 
-        + skin.splashs.link 
-        + "alt=\"\"><h3>" 
-        + skin.splashs.name 
-        + "</h3></div>"));
-    });
+        //DIV difficult
+        divDiffCarte.append($("<p>Difficulté : " + champion.DIFFICULTY + "</p>"));
+        divDiffCarte.append($("<p>Attaque : " + champion.ATK + "</p>"));
+        divDiffCarte.append($("<p>Défense : " + champion.DEF + "</p>"));
+        divDiffCarte.append($("<p>Magie : " + champion.MAG + "</p>"));
 
-    //DIV spells
-    champion.spells.forEach((spell) =>  {
-        divSpellsCarte.append($("<div><div><img src=\"" 
-        + spell.link 
-        + "alt=\"\"><h3>" 
-        + spell.name 
-        + "</h3></div><p>" 
-        + spell.description 
-        + "</p></div>"));
-    });
+        //DIV skins
+        champion.loadings.forEach((loading) =>  {
+            divSkinsCarte.append($("<div><img src=\"" 
+            + loading.link 
+            + "\" alt=\"\"><h3>" 
+            + loading.name 
+            + "</h3></div>"));
+        });
 
-    //SECTION passif
-    champion.passive.forEach((passif) =>  {
+        //DIV spells
+        champion.spells.forEach((spell) =>  {
+            divSpellsCarte.append($("<div><div><img src=\"" 
+            + spell.link 
+            + "\" alt=\"\"><h3>" 
+            + spell.name 
+            + "</h3></div><p>" 
+            + spell.description 
+            + "</p></div>"));
+        });
+
+        //SECTION passif
         secPassifCarte.append($("<div><div><img src=\"" 
-        + passif.link 
-        + "alt=\"\"><h3>" 
-        + spell.name 
+        + champion.passive.link 
+        + "\" alt=\"\"><h3>" 
+        + champion.passive.name 
         + "</h3></div><p>" 
-        + spell.description 
+        + champion.passive.description 
         + "</p></div>"));
-    });
 
-    //DIV lore
-    divLoreCarte.append($("<p>" + champion.lore + "</p>"))
+        //DIV lore
+        divLoreCarte.append($("<p>" + champion.lore + "</p>"));
+    });
 }
