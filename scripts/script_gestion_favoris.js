808349718a27f369cc4a2980a429718f7c67625e
Jordan <jordan.josserand@free.fr>
2022-03-31 15:50:32 +0200

    Merge

--- scripts/script_gestion_favoris.js
+++ scripts/script_gestion_favoris.js
@@ -29,6 +29,7 @@ function init(){
         compteurFavoris = i + 1; // Ajoute 1 à l'indice du dernier élément pour pouvoir rajouter dans le localStrorage
     }
     affichageFavoris(); // Affichage des favoris
+
 }
 
 
